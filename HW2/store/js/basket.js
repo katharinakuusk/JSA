8c78d849bab9e3cd20aad3f8b61eb6646ae0eb23
katharinakuusk <katharinakuusk@gmail.com>
2022-10-30 14:04:17 +0100

    fixed the homework

--- HW2/store/js/basket.js
+++ HW2/store/js/basket.js
@@ -1,16 +1,82 @@
-Basket {
+export class Page {
     constructor() {
-        self.items = [];
-        self.total = 0;
+        this.dir = this.getDir()
+        this.products = [
+            {id: 1, title: "Notebook", price: 2000, img: "notebook"}, 
+            {id:2, title: "Mouse", price: 20, img: "mouse"},
+            {id:3, title: "Keyboard", price: 200, img: "keyboard"},
+            {id:4, title: "Gamepad", price: 50, img: "gamepad"},
+            ]
+        this.getImgSrc()
+        this.items =[];
+    }
+    
+    getDir() {
+        let loc = window.location.pathname;
+        let dir = loc.substring(0, loc.lastIndexOf('/'));
+        return dir
+        
+    }
+    
+    getImgSrc() {
+        let prod = this.products.map(item => item.img = `img/${item.img}`);
+        console.log(this.prod);
+    }
+    
+    renderPage() {
+        this.items = this.products.map(item => new Item(item.id, item.title, item.price, item.img));
+        let productsList = this.items.map(item => item.render());
+        document.querySelector('.products-container').innerHTML=productsList.join("");
+    }
+    
+    getTotal() {
+        
+        let total = 0;
+        let items = this.products;
+        
+        for (let i=0; i<items.length; i++) {
+            let productTotal = items[i].price;
+            total += productTotal;
+        }
+        
+        return total
+        
+    }   
+}
+
+class Item {
+    constructor(id, title, price, img) {
+        this.id = id;
+        this.title = title;
+        this.price = price;
+        this.img = img;
+    }
+    
+    render() {
+    return `<div class="product-item">
+                <h3>${this.title}</h3>
+                <img class="img" src="${this.img}">
+                <div "buy-btn__wrap">
+                    <p class="price">${this.price}</p>
+                    <button class="buy-btn">Buy</button>
+                </div>
+            </div>\n`
+    };
+}
+
+class Basket {
+    constructor() {
+        this.items = [];
+        this.total = 0;
     }
     
     // GoodsList
     getTotal() {
         
         total = 0
-        items = self.items
+        items = this.items
         
-        for (let=0; i<items.length; i++) {
+        for (let i=0; i<items.length; i++) {
             productTotal = items[i].price * items[i].number
             total += productTotal
         }
@@ -38,14 +104,4 @@ Basket {
         
     }
     
-}
-
-Item {
-    constructor() {
-        self.name = "item";
-        self.price = 20;
-        self.img = "src";
-        self.discription = "some description";
-        self.number = 2;
-    }
 }
