9fd18e3145b7821dde78f0b7280d1823359227d0
katharinakuusk <katharinakuusk@gmail.com>
2022-11-05 00:26:18 +0100

    fixed after the lecture

--- HW2/store/js/main.js
+++ HW2/store/js/main.js
@@ -1,8 +1,8 @@
-import {Catalogue} from './catalogue.js';
-import {Cart} from './cart.js';
+import {Catalogue, Cart} from './catalogue.js';
+//import {Cart, CartItem} from './cart.js';
 
-const page = new Catalogue();
 const cart = new Cart();
-cart.onPressCart()
-console.log(cart)
+const page = new Catalogue(cart);
+cart.onPressCart();
+console.log(cart);
 console.log(page);
